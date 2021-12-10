b08a9e3c7277d801fc4097b444e6105ada40e5a7
ViktoriaVasilevskaya <ya.vasilevskaya2012@yandex.ru>
2021-12-10 21:44:59 +0300

    Vik

--- server/src/main/java/ru/ifmo/email/server/Server.java
+++ server/src/main/java/ru/ifmo/email/server/Server.java
@@ -10,27 +10,42 @@ import java.util.List;
 
 
     public class Server extends Thread {
-        private ServerSocket ssocket;
+        //private ServerSocket ssocket;
         ///
+        private class ConnectionServer extends Thread implements AutoCloseable {
+            private final Socket socket;
 
-        @Override
-        public void run() {
-            try (ServerSocket ssocket = new ServerSocket(port)) {
-                this.ssocket = ssocket;
+            private ConnectionServer(Socket socket) {
+                this.socket = socket;
+            }
 
-                while (!isInterrupted()) {
-                    final Socket socket = ssocket.accept();
+            @Override
+            public void run() {
+                try (ServerSocket ssocket = new ServerSocket(port)) {
+                    this.ssocket = ssocket;
 
-                    final ConnectionServer connectionServer = new ConnectionServer(socket);
-                    synchronized (connections) {
-                        connections.add(connectionServer);
+                    while (!isInterrupted()) {
+                        final Socket socket = ssocket.accept();
+
+                        final ConnectionServer connectionServer = new ConnectionServer(socket);
+                        synchronized (connections) {
+                            connections.add(connectionServer);
+                        }
+                        connectionServer.start();
                     }
-                    connectionServer.start();
+                } catch (IOException e) {
+                    // Можем пропустить вывод ошибки в консоль, если мы знаем, что это был останов.
+                    if (!isInterrupted()) {
+                        e.printStackTrace();
+                    }
                 }
-            } catch (IOException e) {
-                // Можем пропустить вывод ошибки в консоль, если мы знаем, что это был останов.
-                if (!isInterrupted()) {
-                    e.printStackTrace();
+            }
+            @Override
+            public void close() throws Exception {
+                interrupt();
+                // Аналогично с ServerSocket, метод read не завершится по вызову interrupt().
+                if (socket != null) {
+                    socket.close();
                 }
             }
         }

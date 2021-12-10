040f4c999b1070df94ddc9a6ce5e80a4cd5cd3ba
Denis Cheklov <denis4ik2387@gmail.com>
2021-12-10 21:19:25 +0300

    1

--- server/src/main/java/ru/ifmo/email/server/Server.java
+++ server/src/main/java/ru/ifmo/email/server/Server.java
@@ -1,4 +1,36 @@
 package ru.ifmo.email.server;
 
-public class Server {
-}
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+
+    public class Server extends Thread {
+        private ServerSocket ssocket;
+
+        @Override
+        public void run() {
+            try (ServerSocket ssocket = new ServerSocket(port)) {
+                this.ssocket = ssocket;
+
+                while (!isInterrupted()) {
+                    final Socket socket = ssocket.accept();
+
+                    final ConnectionServer connectionServer = new ConnectionServer(socket);
+                    synchronized (connections) {
+                        connections.add(connectionServer);
+                    }
+                    connectionServer.start();
+                }
+            } catch (IOException e) {
+                // Можем пропустить вывод ошибки в консоль, если мы знаем, что это был останов.
+                if (!isInterrupted()) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
